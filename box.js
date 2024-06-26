ab7d1d647b701b560347b2bc343f53b1f76f2aab
kairoMD <167260675+kairoMD@users.noreply.github.com>
2024-06-26 09:50:58 +0800

    Add files via upload

---
+++ box.js
@@ -0,0 +1,9 @@
+function resetFlexbox(){
+    document.querySelector('fcon').style.flexDirection = "row";
+    document.querySelector('fcon')
+}
+
+function setFlexdirection(direction){
+    document.querySelector('.fcon').style.flexDirection = direction;
+}
+
